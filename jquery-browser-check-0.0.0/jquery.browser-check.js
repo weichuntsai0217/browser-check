f7670eea5c1cd2e8c352418892c23568068d94d3
weichuntsai0217 <weichuntsai0217@gmail.com>
2015-05-12 14:48:21 +0800

    Temp save

--- jquery-browser-check-0.0.0/jquery.browser-check.js
+++ jquery-browser-check-0.0.0/jquery.browser-check.js
@@ -4,14 +4,6 @@
  * Copyright (c) 2015 Jimmy Tsai
  */
 (function($) {
-  //browser version check algorithm:
-    // check if user's browser is the latest:
-    //    1. if yes, don't alert
-    //    2. if no, check cookie:
-    //        1. if cookie is empty, set cookie and alert
-    //        2. if cookie is not empty, check if cookie is expired:
-    //            1. if not expired, do nothing
-    //            2. if expired, set cookie and alert
     var defaults = {
         verReq: {
             'chrome': '42.0.2311.135',
@@ -53,7 +45,7 @@
             'mobile': true
         },
         addcookie: {
-            'customcookie': 'browseralert=noalert;'
+            'customcookie': 'browseralert=noalert'
         }
     };
     var agt = navigator.userAgent.toLowerCase();//alert(agt);
@@ -119,83 +111,99 @@
         },
         notifyUpgrade: function( options ) {
             var settings = $.extend( true, {}, defaults, options);
-            // $( "#one" ).append( JSON.stringify( defaults ) );
-            // $( "#two" ).append( JSON.stringify( settings ) );
-            // alert('yo');alert(settings.verReq['chrome']);
-            // alert(defaults.spr.chrome);
+            if (( settings.device.desktop === false ) && ( settings.device.mobile === false )) {
+                return;
+            } else if ( settings.device.mobile === false ) {
+                if ( $.bcPlugin.isMobile() ) {
+                    return;
+                }
+            } else if ( settings.device.desktop === false ) {
+                if ( !($.bcPlugin.isMobile()) ) {
+                    return;
+                }
+            }
             var browser = $.bcPlugin.getBrowser();
             var version = $.bcPlugin.getVersion().split('.');
             var verreq = settings.verReq[browser].split('.');
-            var min = Math.min(settings.verCpr[browser], ( verreq.length ) );
-            if (version.length < min) {alert(123);return;}
+            var min = Math.min(settings.verCpr[browser], verreq.length, version.length);alert(min);
+            if ( $.bcPlugin.compareVersion(version, verreq, min) ) {
+                if ( !$.bcPlugin.checkCookie( settings.addcookie.customcookie ) ) {
+                    $.bcPlugin.setCookie( settings.addcookie.customcookie );
+                    $.bcPlugin.showUpgradeMsg(settings.showBrowser[browser], settings.verReq[browser], settings.link[browser]);
+                }
+            }
+        },
+        compareVersion: function(version, verreq, min) {
+            // if verreq > version, will return true
+            var bigger;
             for( var i = 0; i < min; i++ ) {
                 if ( parseInt( version[i] ) < parseInt( verreq[i] ) ) {
-                    if ( !checkCookie( settings.addcookie.customcookie ) ) {
-                        setCookie( settings.addcookie.customcookie );
-                        showUpgradeMsg();
-                    }
+                    bigger = true;
+                    break;
+                } else if ( parseInt( version[i] ) > parseInt( verreq[i] ) ) {
+                    bigger = false;
                     break;
                 }
             }
+            return bigger;
+        },
+        checkCookie: function(customcookie) {
+            if ( document.cookie && (document.cookie.indexOf(customcookie) != -1) ) {
+                return true;
+            }
+            return false;
+        },
+        setCookie: function(customcookie) {
+            // If you want to expire cookie by controling time:
+            // Control time to expire cookie start
+            // var interval = 7200; // in seconds
+            // var  t = new Date();
+            // t.setDate( t.getDate() );
+            // t.setSeconds(t.getSeconds()+interval);
+            // expiretime = t.toGMTString();
+            // document.cookie = 'browseralert=noalert; expires='+expiretime+';';
+            // Control time to expire cookie end
 
-            function checkCookie(customcookie) {
-                if ( document.cookie && (document.cookie.indexOf(customcookie) != -1) ) {
-                    return true;
-                }
-                return false;
-            }
-            function setCookie(customcookie) {
-                // If you want to expire cookie by controling time:
-                // Control time to expire cookie start
-                // var interval = 7200; // in seconds
-                // var  t = new Date();
-                // t.setDate( t.getDate() );
-                // t.setSeconds(t.getSeconds()+interval);
-                // expiretime = t.toGMTString();
-                // document.cookie = 'browseralert=noalert; expires='+expiretime+';';
-                // Control time to expire cookie end
-
-                // If you want to expire cookie by closing browser:
-                // Close browser to expire cookie start:
-                document.cookie = customcookie;
-                // Close browser to expire cookie end:
-            }
-            function showUpgradeMsg() {
-                $('body').find('._bg').show();
-                $('body').append('<div ev-class="dialog-browser-update"><div ev-class="dialog-top">貼心提醒,<br>請將您的瀏覽器升級至<br>' + settings.showBrowser[browser] + ' ' + settings.verReq[browser] + ' 以上版本,<br>以取得最佳瀏覽體驗。</div><div ev-class="dialog-bottom"><a ev-class="continue" href=' + settings.link[browser] + ' target="_blank">前往下載最新版</a><a ev-class="continue">繼續瀏覽</a></div></div>');
-                $('[ev-class="dialog-browser-update"]').css({
-                    'width':'220px',
-                    'position':'fixed',
-                    'left':'50%',
-                    'top':'5%',
-                    'margin-left':'-120px',
-                    'background-color':'#5b5b5b',
-                    'padding':'10px',
-                    'line-height':'1.8',
-                    'z-index':'999'
-                });
-                $('[ev-class="dialog-browser-update"]').find('[ev-class="dialog-top"]').css({
-                    'background-color':'#e0e0e0',
-                    'padding':'20px',
-                    'color':'#000'
-                });
-                $('[ev-class="dialog-browser-update"]').find('[ev-class="dialog-bottom"]').css({
-                    'text-align':'center'
-                });
-                $('[ev-class="dialog-browser-update"]').find('[ev-class="continue"]').css({
-                    'cursor':'pointer',
-                    'display': 'inline-block',
-                    'width':'100px',
-                    'margin':'10px 5px 0 5px',
-                    'background-color':'#3cc',
-                    'color':'#fff',
-                    'text-decoration': 'none'
-                });
-                $('[ev-class="dialog-browser-update"]').find('[ev-class="continue"]').on('click', function(){
-                    $('body').find('._bg').hide();
-                    $('[ev-class="dialog-browser-update"]').remove();
-                });
-            }
+            // If you want to expire cookie by closing browser:
+            // Close browser to expire cookie start:
+            document.cookie = customcookie + ';';
+            // Close browser to expire cookie end:
+        },
+        showUpgradeMsg: function(browser, verreq, link) {
+            $('body').find('._bg').show();
+            $('body').append('<div ev-class="dialog-browser-update"><div ev-class="dialog-top">貼心提醒,<br>請將您的瀏覽器升級至<br>' + browser + ' ' + verreq + ' 以上版本,<br>以取得最佳瀏覽體驗。</div><div ev-class="dialog-bottom"><a ev-class="continue" href=' + link + ' target="_blank">前往下載最新版</a><a ev-class="continue">繼續瀏覽</a></div></div>');
+            $('[ev-class="dialog-browser-update"]').css({
+                'width':'300px',
+                'position':'fixed',
+                'left':'50%',
+                'top':'5%',
+                'margin-left':'-150px',
+                'background-color':'#5b5b5b',
+                'padding':'10px',
+                'line-height':'1.8',
+                'z-index':'999'
+            });
+            $('[ev-class="dialog-browser-update"]').find('[ev-class="dialog-top"]').css({
+                'background-color':'#e0e0e0',
+                'padding':'20px',
+                'color':'#000'
+            });
+            $('[ev-class="dialog-browser-update"]').find('[ev-class="dialog-bottom"]').css({
+                'text-align':'center'
+            });
+            $('[ev-class="dialog-browser-update"]').find('[ev-class="continue"]').css({
+                'cursor':'pointer',
+                'display': 'inline-block',
+                'width':'135px',
+                'margin':'10px 5px 0 5px',
+                'background-color':'#3cc',
+                'color':'#fff',
+                'text-decoration': 'none'
+            });
+            $('[ev-class="dialog-browser-update"]').find('[ev-class="continue"]').on('click', function(){
+                $('body').find('._bg').hide();
+                $('[ev-class="dialog-browser-update"]').remove();
+            });
         }
     };
 })(jQuery);
